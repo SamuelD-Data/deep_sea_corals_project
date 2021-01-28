37261c0d505f0fd10d9459b75cae7bc68e80cc4e
Samuel Davila <samuelrdavila@gmail.com>
2021-01-28 09:34:30 -0600

    added doc string

--- wrangle.py
+++ wrangle.py
@@ -2,7 +2,9 @@ from prepare import prep_corals
 import pandas as pd 
 
 def wrangle_corals():
-
+    """
+    No argument needed. Function acquires, prepares and returns coral data ready for exploration.
+    """
     # importing data
     df = pd.read_csv('deep_sea_corals.csv')
 
